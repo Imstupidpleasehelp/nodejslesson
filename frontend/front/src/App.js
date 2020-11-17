850708937bf908ccd2658fd3bcb733cdaef2c39e
dylan <dylanabozarth@gmail.com>
2020-11-16 20:07:32 -0800

    added frontend

---
+++ frontend/front/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>FORM</h1>
+        <input name="name"></input>
+        <button>Submit</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
