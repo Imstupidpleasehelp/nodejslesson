32b82255b1590a4824f8efc0866b99743a691eb9
dylan <dylanabozarth@gmail.com>
2020-11-17 21:22:03 -0800

    successful form dad

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,46 @@
 import "./App.css";
-
+import React, { useState } from "react";
 function App() {
- 
+  const FooBarForm = () => {
+    const handleChange = (e) => {
+      updateFormData({
+        ...formData,
+
+        // Trimming any whitespace
+        [e.target.name]: e.target.value.trim(),
+      });
+    };
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log(formData);
+      // ... submit to API or something
+    };
+    const initialFormData = Object.freeze({
+      username: "",
+      password: "",
+    });
+    const [formData, updateFormData] = React.useState(initialFormData);
+    return (
+      <>
+        <label>
+          Username
+          <input name="username" onChange={handleChange} />
+        </label>
+        <br />
+        <label>
+          Password
+          <input name="password" onChange={handleChange} />
+        </label>
+        <br />
+        <button onClick={handleSubmit}>Submit</button>
+      </>
+    );
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <form method="POST">
-          <label>Name</label>
-          <input type="text" name="name"></input> <label>Email</label>
-          <input type="text" name="email"></input> <label>Message</label>
-          <input type="text" name="message"></input>
-        </form>{" "}
+        <FooBarForm />
       </header>
     </div>
   );
