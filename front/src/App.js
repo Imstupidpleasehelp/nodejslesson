44d8216ed0504319e1525ecad4453dfbade0d796
dylan <dylanabozarth@gmail.com>
2020-11-19 13:37:24 -0800

    correctly sending data

--- front/src/App.js
+++ front/src/App.js
@@ -2,47 +2,16 @@ import "./App.css";
 import React from "react";
 function App() {
   const FooBarForm = () => {
-    const handleChange = (e) => {
-      updateFormData({
-        ...formData,
-
-        // Trimming any whitespace
-        [e.target.name]: e.target.value.trim(),
-        
-
-      });
-    };
-
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      console.log(formData);
-      var formDataString = JSON.stringify(formData);
-      // ... submit to API or something
-     // download(formDataString, 'json.txt', 'text/plain');
-    };
-    const initialFormData = Object.freeze({
-      username: "",
-      password: "",
-    });
-    function download(formDataString, fileName, contentType) {
-      var a = document.createElement("a");
-      var file = new Blob([formDataString], {type: contentType});
-      a.href = URL.createObjectURL(file);
-      a.download = fileName;
-      a.click();
-      
-  }
-  
-    const [formData, updateFormData] = React.useState(initialFormData);
-    return (
+   return (
       <>
-      <form action="http://localhost:8080/example" method="POST">
+     <form action="http://localhost:8080/example" method="POST">
   First name: <input type="text" name="fname" /><br />
   Last name: <input type="text" name="lname" /><br />
  <button type="submit">Send to backend</button>
 </form>
+
       </>
-    );
+   );
   };
   return (
     <div className="App">
